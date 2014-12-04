9474e2be8c90b0aab6cb5ca296e2548cf93ced0b
Evan Pavlica <evan@xicreative.net>
2014-12-04 10:28:26 -0800

    Cleanup unused files

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Spree::Core::Engine.routes.draw do
   namespace :admin do
-    # get '/prizes/candidates'            => 'prizes/candidates#index'
-    # get '/prizes/candidates/:id/edit'   => 'prizes/candidates#edit',    as: 'edit_candidate'
-    # get '/prizes/candidates/:id/winner' => 'prizes/candidates#winner',  as: 'winner_candidate'
-    # patch '/prizes/candidates/:id'      => 'prizes/candidates#update',  as: 'prize_update_candidate'
-    # delete '/prizes/candidates/:id'     => 'prizes/candidates#destroy', as: 'prize_delete_candidate'
-
     patch '/prizes/:id/winner' => 'prizes#winner', as: 'prize_winner'
 
     resources :prizes do
