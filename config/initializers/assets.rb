9647c866e387cf6ce61f508007ba02b1637919f7
Evan Pavlica <evan@xicreative.net>
2015-05-22 17:05:45 -0700

    Fix assets & dependencies

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += %w( DateTimePicker.min.js DateTimePicker-ltie9.min.css DateTimePicker.min.css flipclock.min.js DateTimePicker-ltie9.min.js )
+Rails.application.config.assets.precompile += %w( flipclock.css DateTimePicker.min.js DateTimePicker-ltie9.min.css DateTimePicker.min.css flipclock.min.js DateTimePicker-ltie9.min.js prizes.css )
